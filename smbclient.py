652c44dfb4b97433ffd76446d4975ebafcf004a8
Clovis Fabricio <mercurial.m.nosklo@0sg.net>
2010-03-17 16:05:58 -0300

    Allowing read-only files in the regex. Thanks to Igor Katson.

--- smbclient.py
+++ smbclient.py
@@ -66,7 +66,7 @@ $
 _file_re = re.compile(r"""
 \s{2}               # file lines start with 2 spaces
 (.*?)\s+            # capture filename non-greedy, eating remaining spaces
-([ADHS]+)           # capture file mode
+([ADHSR]+)          # capture file mode
 \s+                 # after the mode you can have any number of spaces
 (\d+)               # file size
 \s+                 # spaces after file size
